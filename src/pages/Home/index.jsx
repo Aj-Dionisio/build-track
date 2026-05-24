f717c42fe2d1e03576c53810682861bb68a2c4b8
Antonio <jorge_11000@hotmail.com>
2026-05-24 18:28:13 -0300

    80% do styles da home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,8 @@
 import './style.css'
 import PROJECT from '../../assets/make-plant.avif'
+import styles from '../../components/styles.module.css'
+
+
 
 function Home() {
 
@@ -7,43 +10,48 @@ function Home() {
     <div className='page'>
       <div className='container'>
 
-        <nav className='navContent'>
-          <a href="">Home</a>
-          <a href="">Dashboard Principal </a>
-          <a href="">Projetos</a>
-          <a href="">Relatórios</a>
-          <button className='loginButton' type='button'>Login</button>
-          <button className='createButton' type='button'>Criar conta</button>
-
+        <nav className={styles.navContent}>
+          <div className={styles.logo}>logo aqui</div>
+          <div className={styles.links}>
+            <a href="">Home</a>
+            <a href="">Dashboard Principal </a>
+            <a href="">Projetos</a>
+            <a href="">Relatórios</a>
+          </div>
+          <div className={styles.login}>
+            <button className={styles.navButton} type='button'>Login</button>
+            <button className={styles.navButton} type='button'>Criar conta</button>
+          </div>
         </nav>
+        <div className={styles.textContent}>
+          <div className={styles.text}>
+            <h1>
+              Tecnologia e Engenharia <br></br> <span> trabalhando juntas</span>
+            </h1>
+            <p>
+              Gerencie obras, equipes e cronogramas em tempo real com nossa plataforma SaaS. Decisões baseadas em dados para a construção civil do futuro.
+            </p>
 
-        <div className='text'>
-          <h1>
-            Tecnoligiia e Engenharia <br></br>trabalhando juntas
-          </h1>
-          <p>
-            Gerencie obras, equipes e cronogramas em tempo real com nossa plataforma SaaS. Decisões baseadas em dados para a construção civil do futuro.
-          </p>
+            <button className={styles.textButton} type='button'>
+              Ver Dashboard
+            </button>
 
-          <button type='button'>
-            Ver Dashboard
-          </button>
+            <button className={styles.textButton} type='button'>
+              Ver Projetos
+            </button>
 
-          <button type='button'>
-            Ver Projetos
-          </button>
-
-        </div>
-
-        <div className='image'>
-          <img src={PROJECT} />
-
+          </div>
+          <div className={styles.containerImg}>
+            <div className={styles.image}>
+              <img src={PROJECT} />
+            </div>
+          </div>
         </div>
 
       </div>
 
-      <section>
-        <div className='sectionContent'>
+      <section className={styles.sectionContent}>
+        <div className={styles.information }>
           <div>
             <h1>Visão Geral da Operação</h1>
             <p>Monitore os principais indicadores de desempenho de todos os seus projetos em tempo real.</p>
@@ -75,16 +83,16 @@ function Home() {
 
         </div>
       </section>
-      <section>
-        <div >
+      <section className={styles.projectInfo}>
+        <div className= {styles.projectText}>
           <div>
             <h1>Projetos em Destaque</h1>
             <p>Acompanhamento detalhado das principais obras.</p>
           </div>
-        
+
           <div>
-          <button className='left'></button>
-          <button className='right'></button>
+            <button className='left'>esquerda</button>
+            <button className='right'>direita</button>
           </div>
         </div>
 
@@ -94,8 +102,8 @@ function Home() {
           <div className='divCard'></div>
         </div>
 
-         <button>Ver todos os Projetos</button>
-        
+        <button>Ver todos os Projetos</button>
+
       </section>
       <section>
         <div>
