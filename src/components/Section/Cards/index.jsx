9218c820da8ce602ba82454dd205aea4b8c53d92
Antonio Girotto <jorge_11000@hotmail.com>
2026-05-28 09:23:12 -0300

    Atualizar o index.jsx
    Modificação cards section

--- src/components/Section/Cards/index.jsx
+++ src/components/Section/Cards/index.jsx
@@ -3,10 +3,11 @@ import Styles from './styles.module.css'
 function Card() {
     return (
         <div className={Styles.container}>
-            <div className={Styles.sup}>
+            <div className={Styles.geral}>
                 <div className={Styles.text}></div>
                 <div className={Styles.img}></div>
             </div>
+<div className={Styles.info}</div>
             
 
         </div >
